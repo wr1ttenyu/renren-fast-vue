6588e79afddbc509eae18aed989b69521ed3cecc
wr1ttenyu <a198842519>
2020-06-30 14:46:43 +0800

    add category delete and create

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <el-tree
-      :data="category"
+      :data="categories"
       :props="defaultProps"
       :expand-on-click-node="false"
       show-checkbox
       node-key="catId"
+      :default-expanded-keys="expandKey"
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -25,6 +26,30 @@
         </span>
       </span>
     </el-tree>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form ref="form" :model="category" label-width="80px">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name"></el-input>
+        </el-form-item>
+        <el-form-item label="父ID">
+          <el-input v-model="category.parentCid" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="层级">
+          <el-input v-model="category.catLevel" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="是否展示">
+          <el-input v-model="category.showStatus"></el-input>
+        </el-form-item>
+        <el-form-item label="排序">
+          <el-input v-model="category.sort"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -40,7 +65,10 @@ export default {
   //监控data中的数据变化
   data() {
     return {
-      category: [],
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      dialogVisible: false,
+      categories: [],
+      expandKey: [],
       defaultProps: {
         children: "children",
         label: "name"
@@ -55,19 +83,58 @@ export default {
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get"
       }).then(({ data }) => {
-        this.category = data.data;
+        this.categories = data.data;
       });
     },
-    append(data) {},
+
+    // 打开新增对话框
+    append(data) {
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.showStatus = 1;
+      this.category.sort = 0;
+    },
 
     remove(node, data) {
       var ids = [data.catId];
+      this.$confirm(`是否删除[${data.name}]分类?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            this.$message({
+              message: "菜单删除成功",
+              type: "success"
+            });
+            this.getCategoryData();
+            // 设置默认需要展开的菜单
+            this.expandKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
+    },
+
+    addCategory() {
       this.$http({
-        url: this.$http.adornUrl("/product/category/delete"),
+        url: this.$http.adornUrl("/product/category/save"),
         method: "post",
-        data: this.$http.adornData(ids, false)
+        data: this.$http.adornData(this.category, false)
       }).then(({ data }) => {
-        console.log("删除成功");
+        this.$message({
+          message: "菜单保存成功",
+          type: "success"
+        });
+
+        this.getCategoryData();
+        // 设置默认需要展开的菜单
+        this.expandKey = [this.category.parentCid];
       });
     }
   },
